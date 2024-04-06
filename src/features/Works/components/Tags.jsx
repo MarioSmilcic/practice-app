94aab8b551c2c07a9b110e1cea79db5141788504
Mario <mariosmilcic@gmail.com>
2024-04-06 18:18:27 +0200

    added map method in tags

--- src/features/Works/components/Tags.jsx
+++ src/features/Works/components/Tags.jsx
@@ -2,14 +2,20 @@ import React from "react";
 import "../Works.css";
 import Button from "../../../components/Button/Button";
 
+const tags = [
+  { id: 1, text: "All" },
+  { id: 2, text: "UI/UX DESIGN" },
+  { id: 3, text: "HTML & CSS" },
+  { id: 4, text: "REACT JS" },
+  { id: 5, text: "NODE JS" },
+];
+
 const Tags = () => {
   return (
     <div className="tags">
-      <Button text="All" />
-      <Button text="UI/UX DESIGN" />
-      <Button text="HTML & CSS" />
-      <Button text="REACT JS" />
-      <Button text="NODE JS" />
+      {tags.map((tag) => (
+        <Button key={tag.id} text={tag.text} />
+      ))}
     </div>
   );
 };
